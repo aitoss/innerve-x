a63d2d480494907e8ea4abe6d4ea38b3e24796ae
Shashwat-Trivedi <shashwatrivedi2005@gmail.com>
2025-11-02 01:05:34 +0530

    basic ui of clock

--- src/app/testshashwat/page.tsx
+++ src/app/testshashwat/page.tsx
@@ -1,10 +1,8 @@
 import CountdownTimerCard from "@/components/layout/countdownTimerCard";
-import CountdownTimer from "@/utils/countdownTimer";
 
 export default function TestShashwatPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <CountdownTimer eventdate={new Date("2026-01-30")} />
       <CountdownTimerCard />
     </main>
   );
--- src/utils/countdownTimer.tsx
+++ src/utils/countdownTimer.tsx
@@ -6,7 +6,7 @@ type Props = {
     eventdate: Date | string | number;
 };
 
-export default function CountdownTimer(props: Props) {
+const CountdownTimer = (props: Props) => {
     const { eventdate } = props as Props;
     const targetDate = eventdate instanceof Date ? eventdate : new Date(eventdate);
 
@@ -15,7 +15,7 @@ export default function CountdownTimer(props: Props) {
     const [timeRemaining, setTimeRemaining] = useState<number>(getRemaining());
 
     useEffect(() => {
-        setTimeout(() => setTimeRemaining(getRemaining()), 1000);
+        setTimeout(() => setTimeRemaining(getRemaining()), 1000 );
         // update every second and clean up on unmount or if targetDate changes
     }, [timeRemaining]);
 
@@ -25,12 +25,21 @@ export default function CountdownTimer(props: Props) {
         const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
         const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+        // const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
-        return { days, hours, minutes, seconds };
+        return { days, hours, minutes };
     };
 
-    const { days, hours, minutes, seconds } = getFormattedTime(timeRemaining);
+    const { days, hours, minutes } = getFormattedTime(timeRemaining);
 
-    return  [ days , hours , minutes , seconds ]
-}
+    return (
+        <>
+        <div className="text-[40px] text-white [text-shadow:0_3.558px_0_#000]">
+            {days}D {hours}H {minutes}M 
+        </div>
+            
+        </>
+    );
+};
+
+export default CountdownTimer;
