1b5b8afea85a2c693e8a4e9319c0df52061f946d
Shashwat-Trivedi <shashwatrivedi2005@gmail.com>
2025-11-21 00:33:10 +0530

    refactor: Comment out unused DevfolioButton script and update button implementation

--- src/components/ui/DevfolioButton.tsx
+++ src/components/ui/DevfolioButton.tsx
@@ -1,35 +1,36 @@
-'use client';
+"use client";
 import { useEffect } from "react";
 
+//export default function DevfolioButton() {
+//useEffect(() => {
+//  const script = document.createElement("script");
+//  script.src = "https://apply.devfolio.co/v2/sdk.js";
+//  script.async = true;
+ // script.defer = true;
+ // document.body.appendChild(script);
+//
+ // return () => {
+//    document.body.removeChild(script);
+//  };
+//}, []);
+
+//return (
+ // <div
+//    className="apply-button"
+ //   data-hackathon-slug="innerve-x"
+//    data-button-theme="light"
+ //   style={{ height: "44px", width: "312px" }}
+ // ></div>
+//);
+//}
+
 export default function DevfolioButton() {
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.src = 'https://apply.devfolio.co/v2/sdk.js';
-        script.async = true;
-        script.defer = true;
-        document.body.appendChild(script);
 
-        return () => {
-            document.body.removeChild(script);
-        }
-    }, []);
-
-    return (
-
-        <div
-            className="apply-button"
-            data-hackathon-slug="innerve-x"
-            data-button-theme="light"
-            style={{ height: '44px', width: '312px' }}
-        ></div>
-
-
-
-
-    );
+    const handleClick = () => {
+        window.open('https://unstop.com/o/PvyFs2j?lb=7ZSlAfI&utm_medium=Share&utm_source=WhatsApp', '_blank');}
+  return (
+    <button onClick={handleClick} className="flex text-stroke-h4 justify-center items-center text-[18px] sm:text-[20px] md:text-[22px] lg:text-[32px] w-fit py-2 px-12 bg-[linear-gradient(180deg,#FFEA92_1%,#FFFABC_20%,#FFE673_65%,#FFC610_94%)] text-white [text-shadow:0_1.2px_0_#000] sm:[text-shadow:0_1.8px_0_#000] md:[text-shadow:0_2.5px_0_#000] lg:[text-shadow:0_3.558px_0_#000]  border-b-4 border-b-[#EC8F00] border-t-4 border-t-[#ffffff] rounded-xl ursor-pointer transition-transform duration-200 hover:scale-[1.01] active:scale-95">
+      Apply with unstop
+    </button>
+  );
 }
-
-// <button className="relative flex justify-center items-center  gap-6 px-6 p-2 text-white  w-fit  shadow-[0_3px_0_#000] text-xl  md:text-2xl  font-medium border-t-4 border-b-4 border-b-[#EC8F01]  border-t-[#FFF] text-shadow  rounded-xl " style={{ background: 'linear-gradient(180deg, #FFEA92 1%, #FFFABC 20%, #FFE673 65%, #FFC610 94%)' }} >
-//     <Image src={Devfolio} alt="Devfolio Logo" className=" " />
-//     Apply with Devfolio
-// </button>
