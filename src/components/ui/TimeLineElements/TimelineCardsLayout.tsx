fd438fbf8a1f80b912c16262ae2bb9ff11f43b0a
Shashwat-Trivedi <shashwatrivedi2005@gmail.com>
2026-01-08 00:19:57 +0530

    feat: update announcement date for shortlisted teams in timeline card

--- src/components/ui/TimeLineElements/TimelineCardsLayout.tsx
+++ src/components/ui/TimeLineElements/TimelineCardsLayout.tsx
@@ -54,7 +54,7 @@ const timelineData: TimelineCardData[] = [
   {
     cardNumber: 3,
     cardHeading: "Announcement of Shortlisted Teams",
-    cardLine1: "Shortlisted teams will be announced on January 7, 2026.",
+    cardLine1: "Shortlisted teams will be announced on January 9, 2026.",
     cardLine2: "Check back to see if your team advances.",
     position: "right",
     yPosition: { base: 1100, xs: 1140, sm: 1120, md: 1160, lg: 1180, xl: 1180 },
