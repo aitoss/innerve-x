49cc3bfaf0562a48f815358653c8ba271862fff0
Shashwat-Trivedi <shashwatrivedi2005@gmail.com>
2026-01-08 21:10:54 +0530

    feat: update brochure button link and change button label to 'Results'

--- src/components/ui/BrochureButton.tsx
+++ src/components/ui/BrochureButton.tsx
@@ -4,7 +4,7 @@ import ButtonSound from "../../utils/ButtonClickedSound"
 export default function BrochureButton() {
 const handleClick = () => {
     ButtonSound();
-    window.open('https://drive.google.com/file/d/1lpJ2wNF4iU-eEJ2QFSo6bw4SyxnijO5o/view?usp=drive_link', '_blank');
+    window.open('https://drive.google.com/file/d/https://docs.google.com/document/d/1uMeesybWs7aWRscEMJtW4YFVqXG8gAVpcbvkaJ6XgIQ/edit?tab=t.0-eEJ2QFSo6bw4SyxnijO5o/view?usp=drive_link', '_blank');
 }
 
 
@@ -20,7 +20,7 @@ const handleClick = () => {
 
         <button onClick={handleClick} className="w-full border-2 rounded-xl cursor-pointer transition-transform duration-200 hover:scale-[1.01] active:scale-95" >
             <div className="w-full text-white text-shadow flex justify-center items-end text-4xl lg:text-xl md:md lg:px-10 md:px-6 lg:pt-2 md:pt-1 p-1 px-8 shadow-[0_-3px_0_#fff]   border-b-10 border-[#74C138] border-x-2 border-t-2 bor bg-[#9ED952] rounded-xl  ">
-                Brochure
+                Results
             </div>
         </button>
 
