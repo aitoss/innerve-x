5b60d69dd0b3527c47f9c07d348ec64273c33eab
Pradeep-kumar-py <pk4401128@gmail.com>
2026-01-09 09:47:30 +0530

    feat: update BrochureButton to link to new document and change button label to 'Results'

--- src/components/ui/BrochureButton.tsx
+++ src/components/ui/BrochureButton.tsx
@@ -4,7 +4,7 @@ import ButtonSound from "../../utils/ButtonClickedSound"
 export default function BrochureButton() {
 const handleClick = () => {
     ButtonSound();
-    window.open('https://docs.google.com/document/d/1uMeesybWs7aWRscEMJtW4YFVqXG8gAVpcbvkaJ6XgIQ/edit?tab=t.0', '_blank');
+    window.open('https://docs.google.com/document/d/1uMeesybWs7aWRscEMJtW4YFVqXG8gAVpcbvkaJ6XgIQ/edit?amp;fbclid=PAZXh0bgNhZW0CMTEAc3J0YwZhcHBfaWQMMjU2MjgxMDQwNTU4AAGnet9ycJwGWHNyA8nB7r8Vx2AmSm6g8k9DP2f9M5Fkt6e7psWKQVP0SMR5DY4_aem_ItCrHIZU-qCzFp-WBBkR1g&pli=1&tab=t.0', '_blank');
 }
 
 
